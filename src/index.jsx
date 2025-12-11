4c52f4ec8c400574469207305ca4973df9505373
Muskan Akram <muskanakram041@gmail.com>
2025-12-11 21:10:36 +0500

    Set basename for BrowserRouter to '/little-lemon'

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import './index.css';
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/little-lemon">
       <App />
     </BrowserRouter>
   </React.StrictMode>
